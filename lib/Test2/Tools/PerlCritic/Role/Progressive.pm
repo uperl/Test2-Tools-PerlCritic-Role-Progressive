acbb5ea45c4b1bf84085f69576524847facdd879
Graham Ollis <plicease@cpan.org>
2024-02-12 09:23:18 -0700

    Initial structure

---
+++ lib/Test2/Tools/PerlCritic/Role/Progressive.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Test2::Tools::PerlCritic::Role::Progressive {
+
+  # ABSTRACT: Role to progressively critique Perl code
+}
+
+1;
+
---
+++ t/test2_tools_perlcritic_role_progressive.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Test2::Tools::PerlCritic::Role::Progressive;
+
+ok 1, 'todo';
+
+done_testing;
+
+
